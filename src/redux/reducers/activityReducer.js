53e1f25b4b417d43600572d22d6720c939dc612d
Ojo Temiloluwa Philip <themmyloluwaa@gmail.com>
2020-07-27 13:41:32 +0100

    code clean up for activity reducer

--- src/redux/reducers/activityReducer.js
+++ src/redux/reducers/activityReducer.js
@@ -2,10 +2,8 @@ import {
   ADD_ACTIVITY,
   DELETE_ACTIVITY,
   EDIT_ACTIVITY,
-  GET_ACTIVITY,
   SORT_ASC,
-  SORT_DES,
-  RESET_ACTIVITY
+  SORT_DES
 } from "../actions/types";
 
 const initialState = {
@@ -24,6 +22,7 @@ const activityReducer = (state = initialState, action) => {
         ...state,
         activities: state.activities.filter(data => data.key !== action.key)
       };
+
     case EDIT_ACTIVITY:
       const newArray = [...state.activities];
       const letIndex = newArray.findIndex(
@@ -34,49 +33,26 @@ const activityReducer = (state = initialState, action) => {
         ...state,
         activities: [...newArray]
       };
+
     case SORT_ASC:
       return {
         ...state,
         activities: [
-          ...state.activities.sort((a, b) => {
-            const totalTimeA =
-              new Date(a.startTime).getTime() + new Date(a.endTime).getTime();
-            const totalTimeB =
-              new Date(b.startTime).getTime() + new Date(b.endTime).getTime();
-
-            return new Date(b.createdAt) - new Date(a.createdAt);
-          })
+          ...state.activities.sort(
+            (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+          )
         ]
       };
+
     case SORT_DES:
       return {
         ...state,
         activities: [
-          ...state.activities.sort((a, b) => {
-            const totalTimeA =
-              new Date(a.startTime).getTime() + new Date(a.endTime).getTime();
-            const totalTimeB =
-              new Date(b.startTime).getTime() + new Date(b.endTime).getTime();
-
-            return new Date(a.createdAt) - new Date(b.createdAt);
-          })
+          ...state.activities.sort(
+            (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+          )
         ]
       };
-    case GET_ACTIVITY: {
-      return {
-        ...state,
-        activity: state.activities.find(
-          activityValue => activityValue.key === action.key
-        )
-      };
-    }
-
-    case RESET_ACTIVITY: {
-      return {
-        ...state,
-        activity: {}
-      };
-    }
 
     default:
       return { ...state };
