e859809ef41c9c8a5f004f75e4d3d1925fa882ee
Ojo Temiloluwa Philip <themmyloluwaa@gmail.com>
2020-07-27 13:42:28 +0100

    code clean up for store initialisation

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,7 @@ import activityReducer from "./reducers/activityReducer";
 import settingsReducer from "./reducers/settingsReducer";
 import { persistReducer, persistStore } from "redux-persist";
 import AsyncStorage from "@react-native-community/async-storage";
-import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
-import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
+import hardSet from "redux-persist/lib/stateReconciler/hardSet";
 const persisConfig = {
   key: "root",
   storage: AsyncStorage,
