98200ada093844315b9731bb118828f92d1f67b2
Ojo Temiloluwa Philip <themmyloluwaa@gmail.com>
2020-07-27 13:21:14 +0100

    code clean up for home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,83 +1,15 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { themeStyle, defaultAppStyle } from "../utils/appStyles";
+import React from "react";
 import Layout from "../components/Layout";
-import { ButtonGroup } from "react-native-elements";
 import HeadingComponent from "../components/HeadingComponent";
-import ActiveComponent from "../components/ActiveComponent";
-import CompletedComponent from "../components/CompletedComponent";
+import ActivityComponent from "../components/ActivityComponent";
 
-const renderComponent = (index, navigation) => {
-  if (index === 0) {
-    return <ActiveComponent navigation={navigation} />;
-  } else {
-    return <CompletedComponent navigation={navigation} />;
-  }
-};
-const HomeScreen = ({ navigation, ...props }) => {
-  const [index, setIndex] = useState(0);
-  //   const [counter, setCounter] = useState(0);
-
-  const buttons = ["Active", "Completed"];
+const HomeScreen = ({ navigation }) => {
   return (
     <Layout>
       <HeadingComponent />
-      {/* <ButtonGroup
-        onPress={i => {
-          setIndex(i);
-        }}
-        selectedIndex={index}
-        buttons={buttons}
-        containerStyle={{
-          height: 48,
-          width: "100%",
-          backgroundColor: themeStyle.textColor
-        }}
-        selectedButtonStyle={{
-          backgroundColor: defaultAppStyle.primaryColor
-
-          // borderBottomWidth: defaultAppStyle.whiteColor
-        }}
-        selectedTextStyle={{
-          color: defaultAppStyle.whiteColor
-        }}
-        textStyle={{
-          fontWeight: "bold",
-          fontSize: 14
-        }}
-      /> */}
-      <ActiveComponent navigation={navigation} />
-      {/* <Text
-        style={{
-          color: "#fff"
-        }}
-      >
-        counter:{counter}
-      </Text>
-      <TouchableOpacity onPress={() => setCounter(counter + 1)}>
-        <Text
-          style={{
-            color: "#fff"
-          }}
-        >
-          Press me
-        </Text>
-      </TouchableOpacity> */}
+      <ActivityComponent navigation={navigation} />
     </Layout>
   );
 };
 
-// const mapStateToProps = state => {
-//   return {
-//     data: {
-//       activities: state.activitiesReducer.activities,
-//       completed: state.activitiesReducer.activities
-//     }
-//   };
-// };
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     delete: key => dispatch(deleteActivity(key))
-//   };
-// };
 export default HomeScreen;
